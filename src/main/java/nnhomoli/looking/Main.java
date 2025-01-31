dffa3621d2dc59cc9608e9fc7e285aae7f938893
nnHomoli <mrfreedoma271@gmail.com>
2025-01-31 19:35:14 +0700

    rebrand

--- src/main/java/nnhomoli/looking/Main.java
+++ src/main/java/nnhomoli/looking/Main.java
@@ -1,19 +1,18 @@
-package turniplabs.examplemod;
+package nnhomoli.looking;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class ExampleMod implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
-    public static final String MOD_ID = "examplemod";
+public class Main implements ModInitializer, RecipeEntrypoint, GameStartEntrypoint {
+    public static final String MOD_ID = "looking";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("initialized.");
     }
 
 	@Override
---
+++ src/main/java/nnhomoli/looking/mixins/test.java
@@ -0,0 +1,4 @@
+package nnhomoli.looking.mixins;
+
+public class test {
+}
